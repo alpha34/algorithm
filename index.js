2888861f68255695c7e72f6d2aa2e6fe90cc8ae3
hongki <34434577+alpha34@users.noreply.github.com>
2024-08-07 14:54:16 +0000

    [백트래킹]0만들기 연습

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
 let fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().split("\n").map(Number);
+// console.log(input);
 
-// console.log(input[2]);
-
-let cnt = input[0];
-
-let temp = ["", "+", "-"];
-let n;
 let arr = [];
-for (let i = 1; i <= cnt; i++) {
+let n;
+for (let i = 1; i <= input[0]; i++) {
   n = input[i];
 
   arr = [];
   for (let i = 1; i <= n; i++) {
     arr.push(i);
   }
-
   dfs([], 0);
   console.log("");
 }
 
-// console.log(arr);
-function dfs(temp2, depth) {
-  if (depth == n - 1) {
-    let str = "";
-    for (let i = 0; i < n - 1; i++) {
-      str += arr[i] + temp2[i];
-    }
-    str += arr[n - 1];
+function dfs(propsArr, depth) {
+  let answer = "";
+  for (let i = 0; i < n - 1; i++) {
+    answer += arr[i] + propsArr[i];
+  }
+  answer += arr[n - 1];
 
-    if (eval(str) === 0) {
-      console.log(str);
+  if (depth === n - 1) {
+    if (eval(answer) === 0) {
+      console.log(answer);
     }
-
     return;
   }
 
-  for (let i = 0; i < temp.length; i++) {
-    temp2.push(temp[i]);
-    dfs(temp2, depth + 1);
-    temp2.pop();
+  for (let a of ["", "+", "-"]) {
+    propsArr.push(a);
+    dfs(propsArr, depth + 1);
+    propsArr.pop();
   }
 }
