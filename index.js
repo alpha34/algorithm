bd80e9d8fa6daca4f43829dc7a2b417e622c5b4d
hongki <34434577+alpha34@users.noreply.github.com>
2024-08-08 14:54:13 +0000

    [백트래킹] N과 M(2)

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 let fs = require("fs");
-let input = fs.readFileSync("dev/stdin").toString().split("\n").map(Number);
-// console.log(input);
+let input = fs.readFileSync("dev/stdin").toString().split(" ").map(Number);
+
+// console.log(input[0]);
+// console.log(input[1]);
+
+let N = input[0];
+let M = input[1];
 
 let arr = [];
-let n;
-for (let i = 1; i <= input[0]; i++) {
-  n = input[i];
-
-  arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
-  }
-  dfs([], 0);
-  console.log("");
+for (let i = 1; i <= N; i++) {
+  arr.push(i);
 }
 
+let answer = [];
 function dfs(propsArr, depth) {
-  let answer = "";
-  for (let i = 0; i < n - 1; i++) {
-    answer += arr[i] + propsArr[i];
-  }
-  answer += arr[n - 1];
+  if (depth === M) {
+    console.log(answer);
 
-  if (depth === n - 1) {
-    if (eval(answer) === 0) {
-      console.log(answer);
-    }
     return;
   }
 
-  for (let a of ["", "+", "-"]) {
-    propsArr.push(a);
+  for (let i = 0; i < N; i++) {
+    answer.push(arr[i]);
     dfs(propsArr, depth + 1);
-    propsArr.pop();
+    answer.pop();
   }
 }
+
+dfs(arr, 0);
