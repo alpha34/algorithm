48ee3584e920dcf9b78062fd2252b75e005da391
hongki <34434577+alpha34@users.noreply.github.com>
2024-09-02 14:52:59 +0000

    [2231]

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 let fs = require("fs");
-let input = fs.readFileSync("dev/stdin").toString().split("\n");
-let N = Number(input[0]);
-let arr = [];
-for (let a = 1; a < input.length; a++) {
-  arr.push(input[a].split(" ").map(Number));
-}
-// 커밋 테스트....
-// 방문 여부
-const visited = new Array(N).fill(false).map(() => new Array(N).fill(false));
+let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
-console.log(N);
-console.log(arr[1][3]);
-// console.log(visited);
+console.log(input[0].length);
 
-let min = 0;
-let sum = 0;
-function dfs(arr, depth, start, end) {
-  if (depth === N) {
-    console.log(sum);
-    return;
-  }
-  // console.log(visited);
-  if (!visited[start][end]) {
-    sum += arr[start][end];
+const N = Number(input[0]);
+
+for (let i = 1; i < N; i++) {
+  let sum = i;
+  let str = i.toString();
+
+  console.log("str >>> ", str.length);
+  for (let j = 0; j < str.length; j++) {
+    sum += parseInt(str[j]);
   }
 
-  // i가 스타트 지점
-  for (let i = 0; i < N; i++) {
-    visited[start][end] = false;
-    dfs(arr, depth + 1, end, i);
-    visited[start][end] = true;
+  if (sum === N) {
+    return console.log(i);
   }
 }
-
-dfs(arr, 0, 0, 0);
+return 0;
